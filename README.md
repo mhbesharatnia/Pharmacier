bd82a3f2031746ef4c8b0c630bc66f927be030a3
M.H. Besharatnia <besharatnia@hotmail.com>
2022-01-20 15:55:00 +0330

    ➕ feat - add pharmacy model create new model and add it to database for pharmacies

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ it's a web application (Laravel) that finds the nearest pharmacy.
 step1= copy .env.example to .env
 step2= change .env as you want (two last line is docker ports)
 step3= run ```./vendor/bin/sail up``` command
-step5= enjoy :D
+step4= run ```./vendor/bin/sail artisan migrate``` command
+step5= enjoy 😉
---
+++ database/migrations/2022_01_20_112609_create_pharmacies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePharmaciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pharmacies', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 255);
+            $table->text("address");
+            $table->float("lat");
+            $table->float("lon");
+            $table->string("phone", 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pharmacies');
+    }
+}
