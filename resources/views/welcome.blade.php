7fd7ed7c963675876bf859799c178e734584ae95
M.H. Besharatnia <besharatnia@hotmail.com>
2022-01-20 18:10:19 +0330

    ➕ feat - Send nearest pharmacy data After getting location to send data automatically

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
 <body class="antialiased">
     <p>Click the button to get your coordinates.</p>
     <p id="demo">wait...</p>
+    <div id="mydiv"></div>
 </body>
 <script>
     var x = document.getElementById("demo");
@@ -20,8 +21,32 @@
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
     function showPosition(position) {
-        x.innerHTML = "Latitude: " + position.coords.latitude + 
-        "<br>Longitude: " + position.coords.longitude;
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        x.innerHTML = "Latitude: " + lat + 
+        "<br>Longitude: " + lon;
+        fetch('{{  url('') }}/api/find/' + lat + "/" + lon)  
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+                data = JSON.parse(data);
+                data.list.forEach(function(item){
+                    var mydiv = document.getElementById("mydiv");
+                    var newcontent = document.createElement('div');
+                    newcontent.innerHTML = `<b>name</b> = <span>` + item.name + `</span><br>`
+                    + `<b>address</b> = <span>` + item.address + `</span><br>`
+                    + `<b>phone</b> = <span>` + item.phone + `</span><br><br><br><br><br><br>`
+                    ;
+                    while (newcontent.firstChild) {
+                        mydiv.appendChild(newcontent.firstChild);
+                    }
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                x.innerHTML = "Not found any near Pharmacy."
+        // handle the error
+    });
     }
 </script>
 
