b75e3e639e9297f0569e2fe81e1708c697fae1cb
M.H. Besharatnia <besharatnia@hotmail.com>
2022-01-25 14:03:43 +0330

    feat - calculate distance and 10 limit

--- app/Http/Controllers/PharmacyController.php
+++ app/Http/Controllers/PharmacyController.php
@@ -13,14 +13,33 @@ class PharmacyController extends Controller
     public function findNearest($lat = 35.76004078688983, $lon = 51.416459848639526)
     {
         //find in ~100KM square (in Iran)
-        $pharmacy = Pharmacy::where('lat', '>', $lat - 0.5)
+        $pharmacies = Pharmacy::where('lat', '>', $lat - 0.5)
             ->where('lat', '<', $lat + 0.5)
             ->where('lon', '>', $lon - 0.5)
             ->where('lon', '<', $lon + 0.5)->get();
-        //TODO: sort by distance and limit to 10 selection;
-        if ($pharmacy) {
-            return response(["status" => true, "list" => $pharmacy], 200);
+        if ($pharmacies) {
+            $count = count($pharmacies);
+            if ($count > 10) {
+                foreach ($pharmacies as $pharmacy) {
+                    $pharmacy->dist = PharmacyController::distanceCalc($pharmacy->lat, $pharmacy->lon, $lat, $lon);
+                }
+                $nearestPharmacies = $pharmacies->sortBy('dist', -1);
+                $list = [];
+                for ($i = $count-1; $i >= $count-10; $i--) {
+                    $list[] = $nearestPharmacies[$i];
+                }
+            } else {
+                $list = $pharmacies;
+            }
+            return response(["status" => true, "list" => $list], 200);
         }
-        return response(["status" => false, "errorNumber"=>404, "errorMessage"=>"not found"], 404);
+        return response(["status" => false, "errorNumber" => 404, "errorMessage" => "not found"], 404);
+    }
+
+    private static function distanceCalc(float $p1lat, float $p1lon, float $p2lat, float $p2lon)
+    {
+        $a = $p1lat - $p2lat;
+        $b = $p1lon - $p2lon;
+        return ($a * $a) + ($b * $b);
     }
 }
